4493ca126f7aa659ce34e15f390201e3e89721b0
Hieu Dam <86464713+dhhieu113pro@users.noreply.github.com>
2026-08-26 02:21:28 +0700

    feat: gate Quay on getting started

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { AppShell } from "@/components/app-shell";
+import { GettingStartedView } from "@/components/views/getting-started-view";
+import { useWslc } from "@/lib/wslc/store";
 
 export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
-  return <AppShell />;
+  const onboardingCompleted = useWslc((s) => s.onboardingCompleted);
+  return onboardingCompleted ? <AppShell /> : <GettingStartedView />;
 }
